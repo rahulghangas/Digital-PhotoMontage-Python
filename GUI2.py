f8a89a4bdf7d4b108279b0f2d7d2e586110e5c48
Rahul Ghangas <rahulghangas0@gmail.com>
2019-09-19 06:16:10 +1000

    GUI2 now resets to original image after graohcut. Code cleanup

--- GUI2.py
+++ GUI2.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 image = None
 stop = False
 
+
 class App(Tkinter.Tk): 
     def __init__(self, width=320, height=240): 
         super().__init__()
@@ -18,11 +19,11 @@ class App(Tkinter.Tk):
         self.im_counter = 0
         self.width = width
         self.height = height
-        
+
         self.mask = np.ones((height, width)) * -1
-        
+
         self.colors = ((255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0))
-        
+
         self.images = []
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         for i in range(1,5):
@@ -32,24 +33,24 @@ class App(Tkinter.Tk):
                         (height // 3, width //3), 
                         self.font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
             self.images += [empty]
-            
+
         self.labels = []
         for i in range(len(self.images)):
             im = Image.fromarray(self.images[i])
             imgtk = ImageTk.PhotoImage(image=im) 
             label = Tkinter.Label(self, image=imgtk, borderwidth=2, relief="solid")
             label.photo = imgtk
-            label.bind("<Button-1>", self.print_event)
-            label.bind("<B1-Motion>", self.print_event) 
+            label.bind("<Key>", self.input_event)
+            label.bind("<B1-Motion>", self.input_event) 
             label.grid(row=i//2,column=i%2,padx=0, pady=0)
+            label.focus_set()
             self.labels += [label]
-            
+
         self.button = Tkinter.Button(self, text ="GraphCut", command = self.callback).grid(row = 2, column = 0)
         self.button = Tkinter.Button(self, text ="Save Images", command = self.save_im_callback).grid(row = 2, column = 1)
 
-        self.images_masked = self.images.copy()
-        print("put images")
-        
+        self.images_masked = [np.zeros(self.images[0].shape)] * len(self.images)
+
     def callback(self):
         self.lock.acquire()
         z = photoMontage3.solve(np.array(self.images, dtype=np.int32), np.array(self.mask, dtype=np.int32))
@@ -62,76 +63,70 @@ class App(Tkinter.Tk):
         merged_im = np.zeros(self.images[0].shape, dtype=np.uint8)
         for i in range(len(self.images)):
             merged_im[z == i] = self.images[i][z == i]
-        print(merged_im)
         plt.figure()
         plt.imshow(merged_im)
-        
         plt.show()
+
+        self.cleanup()
+
+    def cleanup(self):
+        for i in range(len(self.images)):
+            self.images_masked[i] = self.encase(self.images[i])
+            im = Image.fromarray(self.images_masked[i])
+            imgtk = ImageTk.PhotoImage(image=im)
+            self.labels[i].configure(image=imgtk)
+            self.labels[i].image = imgtk
+        self.mask = np.ones((self.height, self.width)) * -1
 
     def save_im_callback(self):
         for i in range(len(self.images)):
             cv2.imwrite('./saved_imgs/image_' + str(i) +'.jpg', self.images[i][..., ::-1])
 
-    def print_event(self, event):
+    def input_event(self, event):
         if self.im_counter > 3:
             if str(event.type) == 'Motion':
                 try:
                     if str(event.widget) == '.!label':
-                        im = self.images_masked[0]
-                        im[event.y-2:event.y+2, event.x-2:event.x+2] = self.colors[0]
-                        self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = 0
-                        im = Image.fromarray(im)
-                        imgtk = ImageTk.PhotoImage(image=im)
-                        self.labels[0].configure(image=imgtk)
-                        self.labels[0].image = imgtk
+                        image_index = 0
                     elif str(event.widget) == '.!label2':
-                        im = self.images_masked[1]
-                        im[event.y-2:event.y+2, event.x-2:event.x+2] = self.colors[1]
-                        self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = 1
-                        im = Image.fromarray(im)
-                        imgtk = ImageTk.PhotoImage(image=im)
-                        self.labels[1].configure(image=imgtk)
-                        self.labels[1].image = imgtk
+                        image_index = 1
                     elif str(event.widget) == '.!label3':
-                        im = self.images_masked[2]
-                        im[event.y-2:event.y+2, event.x-2:event.x+2] = self.colors[2]
-                        self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = 2
-                        im = Image.fromarray(im)
-                        imgtk = ImageTk.PhotoImage(image=im)
-                        self.labels[2].configure(image=imgtk)
-                        self.labels[2].image = imgtk
+                        image_index = 2
                     elif str(event.widget) == '.!label4':
-                        im = self.images_masked[3]
-                        im[event.y-2:event.y+2, event.x-2:event.x+2] = self.colors[3]
-                        self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = 3
-                        im = Image.fromarray(im)
-                        imgtk = ImageTk.PhotoImage(image=im)
-                        self.labels[3].configure(image=imgtk)
-                        self.labels[3].image = imgtk
+                        image_index = 3
+
+                    im = self.images_masked[image_index]
+                    im[event.y-2:event.y+2, event.x-2:event.x+2] = self.colors[image_index]
+                    self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = image_index
+                    im = Image.fromarray(im)
+                    imgtk = ImageTk.PhotoImage(image=im)
+                    self.labels[image_index].configure(image=imgtk)
+                    self.labels[image_index].image = imgtk
                 except:
                     pass
         else:
-            if str(event.type) == 'ButtonPress':
+            if str(event.type) == 'KeyPress' and event.char in ('1', '2', '3', '4'):
                 global image
+                image_index = int(event.char) - 1
                 img = cv2.resize(image, (self.width, self.height))
                 img = img[..., ::-1]
-                self.images[self.im_counter] = img
-                self.images_masked[self.im_counter] = self.encase(img)
-                im = Image.fromarray(self.images_masked[self.im_counter])
+                self.images[image_index] = img
+                self.images_masked[image_index] = self.encase(img)
+                im = Image.fromarray(self.images_masked[image_index])
                 imgtk = ImageTk.PhotoImage(image=im) 
-                self.labels[self.im_counter].configure(image=imgtk)
-                self.labels[self.im_counter].image = imgtk
+                self.labels[image_index].configure(image=imgtk)
+                self.labels[image_index].image = imgtk
                 self.im_counter += 1
-    
+ 
     def encase(self, img):
         img_copy = np.copy(img)
         faces_rects = self.haar_cascade_face.detectMultiScale(img_copy
                                                          , scaleFactor = 1.05, minNeighbors = 3);
-        print(faces_rects)
         for (x,y,w,h) in faces_rects:
-            cv2.rectangle(img_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.rectangle(img_copy, (int(x + w/5), int(y + h/5)), (int(x + w - w/5), int(y + h - h/5)), (0, 255, 0), 2)
 
         return img_copy
+
 
 class cvRead (threading.Thread):
     def __init__(self, threadID, name, counter):
