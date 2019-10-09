49ca869c2cc3421bfbd0ddcbff073f31f4a3f39b
Rahul Ghangas <u6157693@anu.edu.au>
2019-10-10 03:18:16 +1100

    Added edge cases and did user validation for GUI. Also added new option to load pre-saved images

--- GUI.py
+++ GUI.py
@@ -1,22 +1,28 @@
-import numpy as np
-import cv2
-import tkinter as Tkinter
-import PIL.ImageTk as ImageTk
-import PIL.Image as Image
 import threading
-import photoMontage3
+import tkinter as Tkinter
+from tkinter import filedialog
+
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image as Image
+import PIL.ImageTk as ImageTk
+from functools import partial
+import os
+
+import photoMontage3
 
 image = None
 stop = False
+low_power_mode = False
 
-
-class App(Tkinter.Tk): 
-    def __init__(self, width=320, height=240): 
+class App(Tkinter.Tk):
+    def __init__(self, width=320, height=240):
         super().__init__()
         self.lock = threading.Lock()
-        self.haar_cascade_face = cv2.CascadeClassifier('haar_cascade/haarcascade_frontalface_default.xml')
-        self.im_counter = 0
+        self.haar_cascade_face = cv2.CascadeClassifier(
+            "haar_cascade/haarcascade_frontalface_default.xml"
+        )
         self.erase_mode = False
         self.cursors = ("", "plus")
         self.width = width
@@ -25,40 +31,73 @@ class App(Tkinter.Tk):
         self.mask = np.ones((height, width)) * -1
 
         self.colors = ((255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0))
+        self.color_names = ('RED', 'GREEN', 'BLUE', 'YELLOW')
 
         self.images = []
         self.font = cv2.FONT_HERSHEY_SIMPLEX
-        for i in range(1,5):
-            string = 'Press ' + str(i) + ' to capture image'
+        for i in range(1, 5):
+            string = "Press " + str(i) + " to capture image"
 
-            empty = cv2.putText(np.zeros((height, width, 3), dtype= np.uint8), string , 
-                        (height // 3, width //3), 
-                        self.font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+            empty = cv2.putText(
+                np.zeros((height, width, 3), dtype=np.uint8),
+                string,
+                (height // 3, width // 3),
+                self.font,
+                0.5,
+                (255, 0, 0),
+                2,
+                cv2.LINE_AA,
+            )
             self.images += [empty]
 
         self.labels = []
         for i in range(len(self.images)):
             im = Image.fromarray(self.images[i])
-            imgtk = ImageTk.PhotoImage(image=im) 
-            label = Tkinter.Label(self, image=imgtk, borderwidth=2, relief="solid")
+            imgtk = ImageTk.PhotoImage(image=im)
+            label = Tkinter.Label(self, image=imgtk, background = self.color_names[i], borderwidth = 1, relief = 'groove')
             label.photo = imgtk
-            label.bind("<Key>", self.input_event)
-            label.bind("<B1-Motion>", self.input_event) 
-            label.grid(row=i//2,column=i%2,padx=0, pady=0)
+            # label.bind("<Key>", self.input_event)
+            label.bind("<B1-Motion>", self.draw_event)
+            label.grid(row=(i // 2) * 2, column=i % 2, ipadx=4, ipady=4)
             label.focus_set()
             self.labels += [label]
 
-        self.button = Tkinter.Button(self, text ="GraphCut", command = self.callback).grid(row = 2, column = 0)
-        self.button2 = Tkinter.Button(self, text ="Save Images", command = self.save_im_callback).grid(row = 2, column = 1)
+        self.key_event_binding = self.bind("<Key>", self.key_press_event)
+        self.buttons = []
+        self.buttons.append(Tkinter.Button(self, text="GraphCut", command=self.callback))
+        self.buttons[-1].grid(row=4, column=0)
+        self.buttons.append(Tkinter.Button(
+            self, text="Save Images", command=self.save_im_callback
+        ))
+        self.buttons[-1].grid(row=4, column=1)
+        self.buttons.append(Tkinter.Button(
+            self, text="Load Image 1", command=partial(self.load_image, 0)
+        ))
+        self.buttons[-1].grid(row=1, column=0)
+        self.buttons.append(Tkinter.Button(
+            self, text="Load Image 2", command=partial(self.load_image, 1)
+        ))
+        self.buttons[-1].grid(row=1, column=1)
+        self.buttons.append(Tkinter.Button(
+            self, text="Load Image 3", command=partial(self.load_image, 2)
+        ))
+        self.buttons[-1].grid(row=3, column=0)
+        self.buttons.append(Tkinter.Button(
+            self, text="Load Image 4", command=partial(self.load_image, 3)
+        ))
+        self.buttons[-1].grid(row=3, column=1)
 
-        self.images_masked = [np.zeros(self.images[0].shape)] * len(self.images)
+        self.images_masked = [None] * len(self.images)
 
     def callback(self):
-        self.lock.acquire()
-        z = photoMontage3.solve(np.array(self.images, dtype=np.int32), np.array(self.mask, dtype=np.int32))
-        self.lock.release()
-        global stop
-        stop = True
+        global low_power_mode
+        for button in self.buttons:
+            button.config(state=Tkinter.DISABLED)
+            button.update()
+        low_power_mode = True
+        z = photoMontage3.solve(
+            np.array(self.images, dtype=np.int32), np.array(self.mask, dtype=np.int32)
+        )
         plt.figure()
         plt.imshow(z)
 
@@ -69,94 +108,136 @@ class App(Tkinter.Tk):
         plt.imshow(merged_im)
         plt.show()
 
-        # self.cleanup()
+        low_power_mode = False
+        
+        for button in self.buttons:
+            button.config(state=Tkinter.NORMAL)
+            button.update()
 
     def cleanup(self):
-        for i in range(len(self.images)):
-            self.images_masked[i] = self.encase(self.images[i])
-            im = Image.fromarray(self.images_masked[i])
-            imgtk = ImageTk.PhotoImage(image=im)
-            self.labels[i].configure(image=imgtk)
-            self.labels[i].image = imgtk
-        self.mask = np.ones((self.height, self.width)) * -1
+        pass
+        
+
+    def flush(self, event):
+        return "break"
 
     def save_im_callback(self):
+        if not os.path.exists('./saved_imgs'):
+            os.makedirs('./saved_imgs')
         for i in range(len(self.images)):
-            cv2.imwrite('./saved_imgs/image_' + str(i) +'.jpg', self.images[i][..., ::-1])
+            if self.images_masked[i] is None:
+                continue
+            cv2.imwrite(
+                "./saved_imgs/image_" + str(i) + ".jpg", self.images[i][..., ::-1]
+            )
 
-    def input_event(self, event):
-        if self.im_counter > 3:
-            if str(event.type) == 'Motion':
-                try:
-                    if str(event.widget) == '.!label':
-                        image_index = 0
-                    elif str(event.widget) == '.!label2':
-                        image_index = 1
-                    elif str(event.widget) == '.!label3':
-                        image_index = 2
-                    elif str(event.widget) == '.!label4':
-                        image_index = 3
-                    
-                    im = self.images_masked[image_index]
-                    if not self.erase_mode:
-                        im[event.y-2:event.y+2, event.x-2:event.x+2] = self.colors[image_index]
-                        self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = image_index
-                    else:
-                        im[event.y-5:event.y+5, event.x-5:event.x+5] = self.images[image_index][event.y-5:event.y+5, event.x-5:event.x+5]
-                        self.mask[event.y-2:event.y+2, event.x-2:event.x+2] = -1
+    def draw_event(self, event):
+        if str(event.type) == "Motion":
+            try:
+                if str(event.widget) == ".!label":
+                    image_index = 0
+                elif str(event.widget) == ".!label2":
+                    image_index = 1
+                elif str(event.widget) == ".!label3":
+                    image_index = 2
+                elif str(event.widget) == ".!label4":
+                    image_index = 3
 
-                    im = Image.fromarray(im)
-                    imgtk = ImageTk.PhotoImage(image=im)
-                    self.labels[image_index].configure(image=imgtk)
-                    self.labels[image_index].image = imgtk
+                if self.images_masked[image_index] is None:
+                    return
 
-                except:
-                    pass
-            elif str(event.type) == 'KeyPress' and event.char == 'e':
-                self.erase_mode = not self.erase_mode
-                for i in range(4):
-                    self.labels[i].config(cursor=self.cursors[int(self.erase_mode)])
-        else:
-            if str(event.type) == 'KeyPress' and event.char in ('1', '2', '3', '4'):
-                global image
-                image_index = int(event.char) - 1
-                img = cv2.resize(image, (self.width, self.height))
-                img = img[..., ::-1]
-                self.images[image_index] = img
-                self.images_masked[image_index] = self.encase(img)
-                im = Image.fromarray(self.images_masked[image_index])
-                imgtk = ImageTk.PhotoImage(image=im) 
+                im = self.images_masked[image_index]
+                if not self.erase_mode:
+                    im[
+                        event.y - 2 : event.y + 2, event.x - 2 : event.x + 2
+                    ] = self.colors[image_index]
+                    self.mask[
+                        event.y - 2 : event.y + 2, event.x - 2 : event.x + 2
+                    ] = image_index
+                else:
+                    im[
+                        event.y - 5 : event.y + 5, event.x - 5 : event.x + 5
+                    ] = self.images[image_index][
+                        event.y - 5 : event.y + 5, event.x - 5 : event.x + 5
+                    ]
+                    self.mask[event.y - 2 : event.y + 2, event.x - 2 : event.x + 2] = -1
+
+                im = Image.fromarray(im)
+                imgtk = ImageTk.PhotoImage(image=im)
                 self.labels[image_index].configure(image=imgtk)
                 self.labels[image_index].image = imgtk
-                self.im_counter += 1
- 
+
+            except:
+                pass
+
+    def key_press_event(self, event):
+        if str(event.type) == "KeyPress" and event.char == "e":
+            self.erase_mode = not self.erase_mode
+            for i in range(4):
+                self.labels[i].config(cursor=self.cursors[int(self.erase_mode)])
+
+        elif str(event.type) == "KeyPress" and event.char in ("1", "2", "3", "4"):
+            global image
+            image_index = int(event.char) - 1
+            img = cv2.resize(image, (self.width, self.height))
+            img = img[..., ::-1]
+            self.images[image_index] = img
+            self.images_masked[image_index] = self.encase(img)
+            im = Image.fromarray(self.images_masked[image_index])
+            imgtk = ImageTk.PhotoImage(image=im)
+            self.labels[image_index].configure(image=imgtk)
+            self.labels[image_index].image = imgtk
+
+    def load_image(self, image_index):
+        filename = filedialog.askopenfilename()
+        if not filename or not os.path.exists(filename):
+            return
+        img = cv2.imread(filename)
+        img = cv2.resize(img, (self.width, self.height))
+        img = img[..., ::-1]
+        self.images[image_index] = img
+        self.images_masked[image_index] = self.encase(img)
+        im = Image.fromarray(self.images_masked[image_index])
+        imgtk = ImageTk.PhotoImage(image=im)
+        self.labels[image_index].configure(image=imgtk)
+        self.labels[image_index].image = imgtk
+
     def encase(self, img):
         img_copy = np.copy(img)
-        faces_rects = self.haar_cascade_face.detectMultiScale(img_copy
-                                                         , scaleFactor = 1.05, minNeighbors = 5)
-        for (x,y,w,h) in faces_rects:
-            cv2.rectangle(img_copy, (int(x + w/5), int(y + h/5)), (int(x + w - w/5), int(y + h - h/5)), (0, 255, 0), 2)
+        faces_rects = self.haar_cascade_face.detectMultiScale(
+            img_copy, scaleFactor=1.05, minNeighbors=5
+        )
+        for (x, y, w, h) in faces_rects:
+            cv2.rectangle(
+                img_copy,
+                (int(x + w / 5), int(y + h / 5)),
+                (int(x + w - w / 5), int(y + h - h / 5)),
+                (0, 255, 0),
+                2,
+            )
 
         return img_copy
 
 
-class cvRead (threading.Thread):
+class cvRead(threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.counter = counter
-    
+
     def run(self):
+        global cap
         cap = cv2.VideoCapture(0)
-        global stop        
+        global stop, image, low_power_mode
         while not stop:
-            global image
-            ret, image = cap.read()
-            
+            ret = cap.grab()
+            if not low_power_mode:
+                ret, image = cap.retrieve()
+
 
 thread1 = cvRead(1, "Thread-1", 1)
 thread1.start()
-app = App() 
+app = App()
 app.mainloop()
 stop = True
